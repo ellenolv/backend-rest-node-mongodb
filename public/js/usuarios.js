ca4afc36243d6cc980137db103d14b77f5e9bef1
ellenolv <ellendeoliveirapereira>
2023-11-27 08:29:49 -0300

    projeto finalizado

--- public/js/usuarios.js
+++ public/js/usuarios.js
@@ -1,61 +1,75 @@
 //const urlBase = 'https://backend-mongodb-pi.vercel.app/api'
-const urlBase = 'http://localhost:4000/api'
-const resultadoModal = new bootstrap.Modal(document.getElementById("modalMensagem"))
-const access_token = localStorage.getItem("token") || null
+const urlBase = 'http://localhost:4000/api';
+const resultadoModal = new bootstrap.Modal(document.getElementById("modalMensagem"));
+const access_token = localStorage.getItem("token") || null;
 
-
-async function carregaUsers() {
-    const tabela = document.getElementById('dadosTabela')
-    tabela.innerHTML = '' 
-    await fetch(`${urlBase}/usuarios`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "access-token": access_token //envia o token na requisição
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-
-            data.forEach(usuarios => {
-
-                tabela.innerHTML += `
-                <tr>
-                   <td>${usuarios.nome}</td>
-                   <td>${usuarios.email}</td>
-
-                   <td>
-                       <button class='btn btn-danger btn-sm' onclick='removeUser("${usuarios._id}")'>🗑 Excluir </button>
-                    </td>           
-                </tr>
-                `
-            })
-        })
-        .catch(error => {
-            document.getElementById("mensagem").innerHTML = `<span class='text-danger'>Erro ao carregar: ${error.message}</span>`
-            resultadoModal.show();
-        });
+// Função que encapsula a chamada de carregaUsers
+function inicializar() {
+    carregaUsers();
 }
 
-async function removeUser(id) {
-    if (confirm('Deseja realmente excluir o usuario?')) {
-        await fetch(`${urlBase}/usuarios/${id}`, {
-            method: "DELETE",
+async function carregaUsers() {
+    const tabela = document.getElementById('dadosTabela');
+    tabela.innerHTML = '';
+
+    try {
+        const response = await fetch(`${urlBase}/usuarios`, {
+            method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "access-token": access_token //envia o token na requisição
             }
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.deletedCount > 0) {
-                    //alert('Registro Removido com sucesso')
-                    carregaUsers() // atualiza a UI
-                }
-            })
-            .catch(error => {
-                document.getElementById("mensagem").innerHTML = `<span class='text-danger'>Erro ao carregar: ${error.message}</span>`
-                resultadoModal.show();
-            });
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        data.forEach(usuarios => {
+            tabela.innerHTML += `
+                <tr>
+                   <td>${usuarios.nome}</td>
+                   <td>${usuarios.email}</td>
+                   <td>
+                       <button class='btn btn-danger btn-sm' onclick='removeUser("${usuarios._id}")'>🗑 Excluir </button>
+                    </td>           
+                </tr>
+            `;
+        });
+    } catch (error) {
+        document.getElementById("mensagem").innerHTML = `<span class='text-danger'>Erro ao carregar: ${error.message}</span>`;
+        resultadoModal.show();
     }
 }
+
+async function removeUser(id) {
+    if (confirm('Deseja realmente excluir o usuario?')) {
+        try {
+            const response = await fetch(`${urlBase}/usuarios/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "access-token": access_token //envia o token na requisição
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erro ao excluir: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+
+            if (data.deletedCount > 0) {
+                carregaUsers(); // atualiza a UI
+            }
+        } catch (error) {
+            document.getElementById("mensagem").innerHTML = `<span class='text-danger'>Erro ao carregar: ${error.message}</span>`;
+            resultadoModal.show();
+        }
+    }
+}
+
+// Chamada da função inicializar quando o DOM estiver completamente carregado
+document.addEventListener("DOMContentLoaded", inicializar);
